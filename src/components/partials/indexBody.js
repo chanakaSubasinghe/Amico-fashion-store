1bd6561241e0a44fa851827884b4f5d500626f2c
chanakaSubasinghe <chanaka.s@icloud.com>
2020-05-07 18:30:03 +0530

    remove contact form

--- src/components/partials/indexBody.js
+++ src/components/partials/indexBody.js
@@ -1,153 +1,166 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-
-// importing components
-import ContactForm from '../user/contact-form'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 // importing custom css and javascript
-import '../../public/css/indexBody.css'
-import '../../public/js/indexBody'
+import "../../public/css/indexBody.css";
+import "../../public/js/indexBody";
 
-// importing images 
-import adidas from '../../public/images/indexBody/adidas.png'
-import amante from '../../public/images/indexBody/amante.png'
-import nike from '../../public/images/indexBody/nike.png'
-import gucci from '../../public/images/indexBody/gucci.png'
+// importing images
+import adidas from "../../public/images/indexBody/adidas.png";
+import amante from "../../public/images/indexBody/amante.png";
+import nike from "../../public/images/indexBody/nike.png";
+import gucci from "../../public/images/indexBody/gucci.png";
 
-import s1 from '../../public/images/indexBody/s1.jpg'
-import s2 from '../../public/images/indexBody/s2.jpg'
-import s3 from '../../public/images/indexBody/s3.jpg'
+import s1 from "../../public/images/indexBody/s1.jpg";
+import s2 from "../../public/images/indexBody/s2.jpg";
+import s3 from "../../public/images/indexBody/s3.jpg";
 
-
-import c1 from '../../public/images/indexBody/c1.jpg'
-import c2 from '../../public/images/indexBody/c2.jpg'
-import c3 from '../../public/images/indexBody/c3.jpg'
-
-
+import c1 from "../../public/images/indexBody/c1.jpg";
+import c2 from "../../public/images/indexBody/c2.jpg";
+import c3 from "../../public/images/indexBody/c3.jpg";
 
 export default class IndexBody extends Component {
-
-    render() {
-
-
-        return (
-            <div>
-
-                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img class="d-block w-100" src={c1}
-                            alt="First slide" />
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block w-100" src={c2}
-                            alt="Second slide" />
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block w-100" src={c3}
-                            alt="Third slide" />
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-                <div class="container">
-                        <div class="newDivider text-center">
-                        <h1 class="ThemeText">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</h1>
-                        </div>
-
-                        <div class="newDivider">
-                        <div class="row">
-
-                            <div class="col-md-8 slideanim">
-                                <a href="/"><img class="col-md-12" src={s1} /></a>
-                            </div>
-                            
-                            <div class="col-md-4 justify-content-center align-self-center ">
-                                <h2 class="text-center text-muted">Lorem ipsum dolor sit</h2>
-                                <div class="text-center">
-                                <button class="btn ThemeBackground" type="submit">Shop now</button>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-
-                        <div class="newDivider">
-                        <div class="row">          
-                            <div class="col-sm-4 justify-content-center align-self-center order-3">
-                                <h2 class="text-center text-muted">Lorem ipsum dolor sit</h2>
-                                <div class="text-center">
-                                <button class="btn ThemeBackground" type="submit">Shop now</button>
-                                </div>
-                            </div>
-
-                            <div class="col-md-8 order-2 order-md-12 slideanim">
-                                <a href="/"><img class="col-md-12" src={s2} /></a>
-                            </div>
-                        </div>
-                        </div>
-
-                        <div class="newDivider">
-                        <div class="row">
-                            <div class="col-md-8 slideanim">
-                                <a href="/"><img class="col-md-12" src={s3} /></a>
-                            </div>
-
-                            <div class="col-sm-4 justify-content-center align-self-center ">
-                                <h2 class="text-center text-muted">Lorem ipsum dolor sit</h2>
-                                <div class="text-center">
-                                <button class="btn ThemeBackground" type="submit">Shop now</button>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-
-                        <div class="newDivider text-center">
-                        <h1 class="ThemeText">Our Partners</h1>
-                        <section class="row newDivider slideanim rounded mx-auto d-block">
-                            <img class="col-md-2" src={adidas} />
-                            <img class="col-md-2" src={amante} />
-                            <img class="col-md-2" src={nike} />
-                            <img class="col-md-2" src={gucci} />
-                        </section>
-                        </div>
-
-
-                        <div class="newDivider text-center">
-                        <h1 class="ThemeText">Contact us on 24 hours</h1>
-                        </div>
-
-                        
-                        <div class="newDivider container-fluid my-4 slideanim">
-                            <ContactForm />
-                        </div>
-
-
-                    <div class="newDivider text-center">
-                        <h1 class="ThemeText">About us</h1>
-                    </div>
-
-                    <div class="newDivider text-muted slideanim">
-                        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Pretium nibh ipsum consequat nisl vel pretium lectus. Nunc id cursus metus aliquam eleifend. Ullamcorper a 
-                        lacus vestibulum sed arcu. Auctor eu augue ut lectus arcu bibendum at varius vel. Orci nulla pellentesque dignissim enim.
-                        Sit amet tellus cras adipiscing. Non odio euismod lacinia at. Amet nulla facilisi morbi tempus. Ut consequat semper viverra
-                        nam libero. Neque sodales ut etiam sit. Odio morbi quis commodo odio aenean. Cras fermentum odio eu feugiat pretium
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Pretium nibh ipsum consequat nisl vel pretium lectus. Nunc id cursus metus aliquam eleifend. Ullamcorper a 
-                        lacus vestibulum sed arcu. Auctor eu augue ut lectus arcu bibendum at varius vel. Orci nulla pellentesque dignissim enim.
-                        Sit amet tellus cras adipiscing. Non odio euismod lacinia at. Amet nulla facilisi morbi tempus. Ut consequat semper viverra
-                        nam libero. Neque sodales ut etiam sit. Odio morbi quis commodo odio aenean. Cras fermentum odio eu feugiat pretium                       
-                        </h5>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div
+          id="carouselExampleFade"
+          class="carousel slide carousel-fade"
+          data-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src={c1} alt="First slide" />
             </div>
-        )
-    }
+            <div class="carousel-item">
+              <img class="d-block w-100" src={c2} alt="Second slide" />
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src={c3} alt="Third slide" />
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleFade"
+            role="button"
+            data-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleFade"
+            role="button"
+            data-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <div class="container">
+          <div class="newDivider text-center">
+            <h1 class="ThemeText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            </h1>
+          </div>
+
+          <div class="newDivider">
+            <div class="row">
+              <div class="col-md-8 slideanim">
+                <a href="/">
+                  <img class="col-md-12" src={s1} />
+                </a>
+              </div>
+
+              <div class="col-md-4 justify-content-center align-self-center ">
+                <h2 class="text-center text-muted">Lorem ipsum dolor sit</h2>
+                <div class="text-center">
+                  <button class="btn ThemeBackground" type="submit">
+                    Shop now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="newDivider">
+            <div class="row">
+              <div class="col-sm-4 justify-content-center align-self-center order-3">
+                <h2 class="text-center text-muted">Lorem ipsum dolor sit</h2>
+                <div class="text-center">
+                  <button class="btn ThemeBackground" type="submit">
+                    Shop now
+                  </button>
+                </div>
+              </div>
+
+              <div class="col-md-8 order-2 order-md-12 slideanim">
+                <a href="/">
+                  <img class="col-md-12" src={s2} />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="newDivider">
+            <div class="row">
+              <div class="col-md-8 slideanim">
+                <a href="/">
+                  <img class="col-md-12" src={s3} />
+                </a>
+              </div>
+
+              <div class="col-sm-4 justify-content-center align-self-center ">
+                <h2 class="text-center text-muted">Lorem ipsum dolor sit</h2>
+                <div class="text-center">
+                  <button class="btn ThemeBackground" type="submit">
+                    Shop now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="newDivider text-center">
+            <h1 class="ThemeText">Our Partners</h1>
+            <section class="row newDivider slideanim rounded mx-auto d-block">
+              <img class="col-md-2" src={adidas} />
+              <img class="col-md-2" src={amante} />
+              <img class="col-md-2" src={nike} />
+              <img class="col-md-2" src={gucci} />
+            </section>
+          </div>
+
+          <div class="newDivider text-center">
+            <h1 class="ThemeText">About us</h1>
+          </div>
+
+          <div class="newDivider text-muted slideanim">
+            <h5>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Pretium nibh ipsum consequat nisl vel pretium lectus. Nunc id
+              cursus metus aliquam eleifend. Ullamcorper a lacus vestibulum sed
+              arcu. Auctor eu augue ut lectus arcu bibendum at varius vel. Orci
+              nulla pellentesque dignissim enim. Sit amet tellus cras
+              adipiscing. Non odio euismod lacinia at. Amet nulla facilisi morbi
+              tempus. Ut consequat semper viverra nam libero. Neque sodales ut
+              etiam sit. Odio morbi quis commodo odio aenean. Cras fermentum
+              odio eu feugiat pretium Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium
+              lectus. Nunc id cursus metus aliquam eleifend. Ullamcorper a lacus
+              vestibulum sed arcu. Auctor eu augue ut lectus arcu bibendum at
+              varius vel. Orci nulla pellentesque dignissim enim. Sit amet
+              tellus cras adipiscing. Non odio euismod lacinia at. Amet nulla
+              facilisi morbi tempus. Ut consequat semper viverra nam libero.
+              Neque sodales ut etiam sit. Odio morbi quis commodo odio aenean.
+              Cras fermentum odio eu feugiat pretium
+            </h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/partials/navBar.js
+++ src/components/partials/navBar.js
@@ -1,116 +1,161 @@
-import React, {Component} from 'react'
-import {Link, Redirect} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
 
 // importing custom css and javascript
-import '../../public/css/navbar.css'
-import collapseFunction from '../../public/js/navbar.js'
+import "../../public/css/navbar.css";
+import collapseFunction from "../../public/js/navbar.js";
 
 // importing images for navbar
-import amico from '../../public/images/navbar/amico.png'
-import phone from '../../public/images/navbar/phone-alt-solid.png'
-import instagram from '../../public/images/navbar/instagram-brands.png'
-import shoppingVan from '../../public/images/navbar/shipping-fast-solid.png'
+import amico from "../../public/images/navbar/amico.png";
+import phone from "../../public/images/navbar/phone-alt-solid.png";
+import instagram from "../../public/images/navbar/instagram-brands.png";
+import shoppingVan from "../../public/images/navbar/shipping-fast-solid.png";
 
-import {isAuthenticated, logout} from '../../auth/index'
+import { isAuthenticated, logout } from "../../auth/index";
 
 export default class NavBar extends Component {
+  // constructor
+  constructor(props) {
+    super(props);
 
-    // constructor 
-    constructor(props) {
-        super(props)
-        
-        this.state ={
-            userObj: isAuthenticated()
-        }
-        // binding functions
-        this.logout = this.logout.bind(this)
-    }
+    this.state = {
+      userObj: isAuthenticated(),
+    };
+    // binding functions
+    this.logout = this.logout.bind(this);
+  }
 
-    // logout function
-    logout(e) {
+  // logout function
+  logout(e) {
+    e.preventDefault();
 
-        e.preventDefault()
+    logout(this.state.userObj, () => {
+      return (window.location = "/");
+    });
+  }
 
-        logout(this.state.userObj, () => {
-            return window.location = '/'
-        });
-    }
-    
-    render() {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-dark bg-dark">
+          <div class=" container row">
+            <small class="text-white">
+              <img src={phone} class="nav-icons" /> +94112345678
+            </small>
+            <small class="text-white">
+              <img src={shoppingVan} class="nav-icons" /> free delivery on
+              island wide
+            </small>
+            <small class="text-white">
+              <Link to="http://www.instagram.com">
+                <img src={instagram} class="nav-icons" />
+              </Link>{" "}
+              follow us
+            </small>
+          </div>
+        </nav>
 
-        return (
-            <div>
-                <nav class="navbar navbar-dark bg-dark">
-                    <div class=" container row">
-                        <small class="text-white"><img src={phone} class="nav-icons" /> +94112345678</small>
-                        <small class="text-white"><img src={shoppingVan} class="nav-icons" /> free delivery on island wide</small>
-                        <small class="text-white"><Link to="http://www.instagram.com"><img src={instagram} class="nav-icons" /></Link> follow us</small>
-
-                    </div>
-                </nav>
-
-                <nav class="navbar navbar-expand-lg navbar-dark" id="main-header">
-                    <div class="container">
-                            <Link class="navbar-brand" to="/"><img class="mx-auto d-block" id="amico-brand" src={amico} /></Link>
-                                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                                    <ul class="navbar-nav">
-
-                                        <li class="nav-item">
-                                            <Link class="nav-link active" to="/items">Shop our products</Link>
-                                        </li>
-
-                                        <form className="form-inline mx-5">
-                                            <input className="form-control form-control-sm mr-2" type="text"
-                                                    placeholder="search category" />
-                                                <button className="btn btn-sm searchButton my-2 my-sm-0"
-                                                        type="submit">Search
-                                                </button>
-                                        </form>
-                                            
-                                    </ul>
-                                </div>
-                                {this.state.userObj ? ( 
-                                <div class="row hideWithCollapse">
-                                
-                                        {this.state.userObj.user.role === 'user' &&
-                                            <div class="d-inline m-2">
-                                                <Link class="m-2"><i class="fa fa-heart NavBar-heart-Icon"></i><span class="badge badge-light"><small>10</small></span></Link>
-                                                <Link><i class="fa fa-shopping-cart NavBar-shopping-cart-Icon"></i><span class="badge badge-light"><small>10</small></span></Link>
-                                            </div>
-                                        }
-
-                               
-
-                                        <div class="d-inline dropdown nav-link">
-                                            <Link class="dropdown-toggle text-light" style={{color: "white", textDecoration: "none"}} to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    Signed In As {this.state.userObj.user.firstName}
-                                                </Link>
-
-                                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                    <Link class="dropdown-item" to="/users/me">Profile</Link>
-                                                    {this.state.userObj.user.role === 'admin'
-                                                        &&  <Link class="dropdown-item" to="/adminPanel">Settings</Link>
-                                                    }
-                                                    {this.state.userObj.user.role === 'storeManager'
-                                                        &&  <Link class="dropdown-item" to="/storeManagerPanel">Settings</Link>
-                                                    }      
-                                                    <div class="dropdown-divider"></div>
-                                                    <Link class="dropdown-item" to="#" onClick={this.logout}>Logout</Link>
-                                                </div>
-                                        </div>  
-                                    </div>
-                                    ) : ( 
-
-                                            <div class="d-inline">
-                                                <Link to="/login" class="hideWithCollapse" style={{color: "white", textDecoration: "none"}}>Login | Join</Link>
-                                            </div>
-
-                                    )} 
-
-                                <button className='navbar-toggler' onClick={collapseFunction} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                    </div>
-                </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark" id="main-header">
+          <div class="container">
+            <Link class="navbar-brand" to="/">
+              <img class="mx-auto d-block" id="amico-brand" src={amico} />
+            </Link>
+            <div
+              class="collapse navbar-collapse justify-content-center"
+              id="navbarSupportedContent"
+            >
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link class="nav-link active" to="/items">
+                    Shop our products
+                  </Link>
+                </li>
+              </ul>
             </div>
-        )
-    }
+            {this.state.userObj ? (
+              <div class="row hideWithCollapse">
+                {this.state.userObj.user.role === "user" && (
+                  <div class="d-inline m-2">
+                    <Link class="m-2">
+                      <i class="fa fa-heart NavBar-heart-Icon"></i>
+                      <span class="badge badge-light">
+                        <small>10</small>
+                      </span>
+                    </Link>
+                    <Link>
+                      <i class="fa fa-shopping-cart NavBar-shopping-cart-Icon"></i>
+                      <span class="badge badge-light">
+                        <small>10</small>
+                      </span>
+                    </Link>
+                  </div>
+                )}
+
+                <div class="d-inline dropdown nav-link">
+                  <Link
+                    class="dropdown-toggle text-light"
+                    style={{ color: "white", textDecoration: "none" }}
+                    to="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Signed In As {this.state.userObj.user.firstName}
+                  </Link>
+
+                  <div
+                    class="dropdown-menu dropdown-menu-right"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <Link class="dropdown-item" to="/users/me">
+                      Profile
+                    </Link>
+                    {this.state.userObj.user.role === "admin" && (
+                      <Link class="dropdown-item" to="/adminPanel">
+                        Settings
+                      </Link>
+                    )}
+                    {this.state.userObj.user.role === "storeManager" && (
+                      <Link class="dropdown-item" to="/storeManagerPanel">
+                        Settings
+                      </Link>
+                    )}
+                    <div class="dropdown-divider"></div>
+                    <Link class="dropdown-item" to="#" onClick={this.logout}>
+                      Logout
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div class="d-inline">
+                <Link
+                  to="/login"
+                  class="hideWithCollapse"
+                  style={{ color: "white", textDecoration: "none" }}
+                >
+                  Login | Join
+                </Link>
+              </div>
+            )}
+
+            <button
+              className="navbar-toggler"
+              onClick={collapseFunction}
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="true"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
