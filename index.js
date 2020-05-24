909d52c2935ae8a679b21e6479a0695699ae2d98
chanakaSubasinghe <chanaka.s@icloud.com>
2020-05-24 18:50:38 +0530

    final production on heroku

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ app.use(wishListRoute);
 
 app.use(commentRoute)
 
-if (process.env.NODE_ENV.trim() === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV.trim() === "production") {
+//   app.use(express.static("client/build"));
+// }
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
